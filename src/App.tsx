777b5c3fb97d2d0a586bb1b78d241aa7bf5e8fb5
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-23 15:01:55 +0200

    style using <Component>.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Component from './components/Component/Component';
 
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
+      <p>Hallo</p>
+      <Component />
     </div>
   );
 }
---
+++ src/components/Component/Component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Component.module.css';
+
+const Component = () => {
+  return (
+    <div className={styles.Component}>
+      <h1>Component</h1>
+      <p>Im the paragraph of the Component</p>
+    </div>
+  );
+};
+
+export default Component;
