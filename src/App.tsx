e1c3c73374085d4b385f1a5e5129d876a9ceabfd
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-23 15:08:36 +0200

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import './App.css';
 import Component from './components/Component/Component';
 
 function App() {
+  const age = 15;
+
   return (
     <div className="App">
       <h1>Test</h1>
       <p>Hallo</p>
-      <Component />
+      {age >= 18 ? (
+        <Component />
+      ) : (
+        <div>
+          Under age
+          <h1>HAHA</h1>
+        </div>
+      )}
     </div>
   );
 }
