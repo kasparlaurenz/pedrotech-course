6c64cb7eaf2bec6557a42d66cc5540608465e0ae
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-23 15:34:36 +0200

    map over an object

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 const Lists = () => {
   const names = ['pedro', 'marc', 'dustin', 'charlie'];
+
+  const users = [
+    { id: 1, name: 'Peter', age: 13 },
+    { id: 2, name: 'Jake', age: 31 },
+    { id: 3, name: 'Marry', age: 24 },
+  ];
+
   return (
     <div className="Lists">
       {names.map((name, key) => {
         return <h1 key={key}>{name}</h1>;
+      })}
+
+      {users.map(user => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <span>{user.age}</span>
+          </div>
+        );
       })}
     </div>
   );
