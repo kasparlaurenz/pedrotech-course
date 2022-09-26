4211c521da2a40a36e1636dea5ee57ae6ac06825
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-27 00:09:43 +0200

    add homepage as page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
