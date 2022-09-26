4427ab66fab552af4a802433eee7ed767737e340
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-27 00:08:12 +0200

    basic router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
     </div>
   );
 }
