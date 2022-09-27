3d3ecb534f76fd8325a87071f3091477c8fa19e1
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-27 12:22:31 +0200

    basic setup for video

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/ChangeProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChangeProfile = () => {
+  return <div>ChangeProfile</div>;
+};
+
+export default ChangeProfile;
