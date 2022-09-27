dee5e8ddc062f80e12f9e4b44fca99169f77c387
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-28 01:54:11 +0200

    use react-query inside component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AppContext, AppContextTypes } from '../App';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const Home = () => {
+  const { data } = useQuery(['catfact'], async () => {
+    return await axios.get('https://catfact.ninja/fact').then(res => res.data);
+  });
+  console.log(data?.fact);
   const { username } = useContext(AppContext) as AppContextTypes;
+
   return (
     <div>
       <h1>Home of</h1>
       <p>{username}</p>
       <Link to="/about">Go to about</Link>
+      <p>{data?.fact}</p>
     </div>
   );
 };
