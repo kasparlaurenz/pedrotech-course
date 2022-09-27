ed595d65468b14dfde6a26e1cfbea23a4b3866c6
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-28 01:26:45 +0200

    setup react query in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export type AppContextTypes = {
   username: string;
@@ -14,20 +15,23 @@ export type AppContextTypes = {
 export const AppContext = createContext<AppContextTypes | null>(null);
 
 function App() {
+  const client = new QueryClient();
   const [username, setUsername] = useState<string>('Laurenz');
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ username, setUsername }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
