0e86994a74e2e96646082564300a6cf08fabc900
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-09-28 16:07:07 +0200

    validate form using yup library

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
+type FormInputs = {
+  fullName: string;
+  email: string;
+  age: number;
+  password: string;
+  confirmedPassword: string;
+};
 const Form = () => {
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(6).max(15).required(),
+    confirmedPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'Passwords dont match')
+      .required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>({ resolver: yupResolver(schema) });
+
+  const onSubmit = handleSubmit(data => console.log(data));
+
   return (
-    <form action="">
-      <input type="text" placeholder="Full Name" />
-      <input type="text" placeholder="Email" />
-      <input type="number" placeholder="Age" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="Full Name" {...register('fullName')} />
+      <p>{errors.fullName?.message}</p>
+      <p>{errors.fullName && <div>Individual Error Message</div>}</p>
+
+      <input type="text" placeholder="Email" {...register('email')} />
+      <input type="number" placeholder="Age" {...register('age')} />
+      <input
+        type="password"
+        placeholder="Password"
+        autoComplete="on"
+        {...register('password')}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        autoComplete="on"
+        {...register('confirmedPassword')}
+      />
       <button type="submit">Submit</button>
     </form>
   );
