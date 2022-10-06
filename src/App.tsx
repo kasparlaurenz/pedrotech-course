07cfd81e485710e8d09af5acf4cdf6adf1c403bc
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-10-06 17:12:21 +0200

    use react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import { useToggle } from './useToggle';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Cat from './components/Cat';
 
 function App() {
+  const queryClient = new QueryClient();
   const [isVisible, toggle] = useToggle(false);
   return (
     <div className="App">
-      <button onClick={toggle}>{isVisible ? 'Hide' : 'Show'}</button>
-      {isVisible && <h1>Hidden Text</h1>}
+      <QueryClientProvider client={queryClient}>
+        <button onClick={toggle}>{isVisible ? 'Hide' : 'Show'}</button>
+        {isVisible && <h1>Hidden Text</h1>}
+        <Cat />
+      </QueryClientProvider>
     </div>
   );
 }
